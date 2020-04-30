9e50b7bfa77d3c48b927a29df9f9627d3b4014eb
parampalanisamy <paramasivam.palanisamy@gmail.com>
2020-04-30 13:20:41 +0100

    Included the CORS configuration to access the APIs from external applications

--- src/main/java/com/phraseandcharacter/utility/PhraseandcharactersApplication.java
+++ src/main/java/com/phraseandcharacter/utility/PhraseandcharactersApplication.java
@@ -10,8 +10,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
 import java.util.Arrays;
@@ -20,6 +25,7 @@ import java.util.Arrays;
 @ComponentScan(basePackages = "com.phraseandcharacter")
 @EntityScan(basePackages = {"com.phraseandcharacter.model"})
 @EnableJpaRepositories(basePackages = {"com.phraseandcharacter.repository"})
+@EnableWebMvc
 public class PhraseandcharactersApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -32,6 +38,16 @@ public class PhraseandcharactersApplication implements CommandLineRunner {
         this.characterService = characterService;
         this.phraseService = phraseService;
 
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
     @Override
